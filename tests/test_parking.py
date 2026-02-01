d5f5c3967fa79f1de2017700ce8b60c64e7c30b4
Nattapong-top <Nattapong.top@hotmail.com>
2026-02-01 16:30:26 +0700

    test parking เกิน 24 ชั่วโมง และ จดปกติ 5 ชั่วโมง

--- tests/test_parking.py
+++ tests/test_parking.py
@@ -8,3 +8,10 @@ def test_parking_hour_should_fail_if_negetive():
     with pytest.raises(ValueError):
         ParkingHour(value=-1)
 
+def test_parking_hour_should_fail_if_24():
+    with pytest.raises(ValueError):
+        ParkingHour(value=25)
+
+def test_parking_hour_nomal_5():
+    hour = ParkingHour(value=5)
+    assert hour.value == 5
