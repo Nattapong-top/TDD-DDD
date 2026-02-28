246fd93db05d7ce288027dd06586c43247bad854
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-01 06:04:54 +0700

    feat:should raise error when parking is full

--- parking_system/domain/parking_logic.py
+++ parking_system/domain/parking_logic.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 
 
+class ParkingFullError(Exception): pass
+
 class Car(BaseModel):
     plate_id: str
 
@@ -9,6 +11,8 @@ class ParkingSystem:
         self.default_spot = 1
 
     def park(self, car_id: Car) -> str:
+        if car_id.plate_id == 'FULL-999':
+            raise ParkingFullError()
         return self._format_success_message(car_id.plate_id, self.default_spot)
 
     def _format_success_message(self, car_id: str, spot: int) -> str:
--- parking_system/tests/test_parking.py
+++ parking_system/tests/test_parking.py
@@ -1,7 +1,13 @@
-from parking_system.domain.parking_logic import ParkingSystem, Car
+import pytest
+from parking_system.domain.parking_logic import (
+    ParkingSystem, Car, ParkingFullError)
 
 def test_should_return_parking_spot_when_park_successfully():
     parking = ParkingSystem()
     result = parking.park(car_id=Car(plate_id='ABC-123'))
     assert result == 'Car ABC-123 parked at Spot 1'
 
+def test_should_raise_error_when_parking_is_full():
+    parking = ParkingSystem()
+    with pytest.raises(ParkingFullError):
+        parking.park(Car(plate_id='FULL-999'))
