fc279952ca161c5a2a9cb9eba9cc8f08eb4f19f9
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-02-28 18:38:33 +0700

    feat: should randomly select a drink between tea and coffee when buying surprise

--- Coffee_Shop/domain/shop_logic.py
+++ Coffee_Shop/domain/shop_logic.py
@@ -2,37 +2,50 @@ from Coffee_Shop.domain.value_object import MoneyTHB, DrinkName
 
 
 class Shop:
-    def __init__(self):
-        self.PRICE = {
-            'Tea': MoneyTHB(value=20.0),
-            'Coffee': MoneyTHB(value=30.0),
-        }
+    PRICE = {
+        'Tea': MoneyTHB(value=20.0),
+        'Coffee': MoneyTHB(value=30.0),
+        'Ovaltine': MoneyTHB(value=15.0),
+    }
 
+    def __init__(self, randomly_select):
+        self._randomly_select = randomly_select
 
     def buy(self, drink_name: DrinkName, payment: MoneyTHB) -> tuple[DrinkName, MoneyTHB]:
-        self._validate_drink(drink_name)
-        change_amount = self._bill(drink_name, payment)
-        return drink_name, MoneyTHB(value=change_amount)
+        final_drink = self._get_final_drink_name(drink_name)
+        self._validate_drink(final_drink)
+        change_amount = self._bill(final_drink, payment)
+        return final_drink, MoneyTHB(value=change_amount)
 
+    def _get_final_drink_name(self, drink_name: DrinkName) -> DrinkName:
+        actual_drink_str = drink_name.value
+        if actual_drink_str == 'Surprise':
+            menu_choice = ['Tea', 'Coffee']
+            actual_drink_str = self._randomly_select(menu_choice)
+        final_drink = DrinkName(value=actual_drink_str)
+        return final_drink
 
     def _bill(self, drink_name: DrinkName, payment: MoneyTHB) -> float:
         drink_price = self.PRICE.get(drink_name.value)
-
         if payment.value < drink_price.value:
             raise MoneyNotEnough()
-
         change_amount = payment.value - drink_price.value
         return change_amount
-
 
     def _validate_drink(self, drink_name: DrinkName):
         if drink_name.value not in self.PRICE:
             raise DrinkNotInMenu()
 
 
-class DrinkNotInMenu(Exception):
-    pass
+class DrinkNotInMenu(Exception): pass
 
 
-class MoneyNotEnough(Exception):
-    pass
+class MoneyNotEnough(Exception): pass
+
+
+class MockRandomlySelect:
+    def __init__(self):
+        self.is_called_with_choices: list[str] = []
+    def __call__(self, choices: list[str]) -> str:
+        self.is_called_with_choices = choices
+        return 'Tea'
--- Coffee_Shop/tests/test_coffee_shop.py
+++ Coffee_Shop/tests/test_coffee_shop.py
@@ -1,11 +1,13 @@
 from pytest import raises
 
-
-from Coffee_Shop.domain.shop_logic import Shop, DrinkNotInMenu,MoneyNotEnough
+from Coffee_Shop.domain.shop_logic import (
+    Shop, DrinkNotInMenu, MoneyNotEnough, MockRandomlySelect)
 from Coffee_Shop.domain.value_object import DrinkName, MoneyTHB
 
+
 def test_should_return_tea_change_30_when_buying_tea_with_50_baht():
-    shop = Shop()
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
 
     order = DrinkName(value='Tea')
     pay = MoneyTHB(value=50.0)
@@ -15,8 +17,10 @@ def test_should_return_tea_change_30_when_buying_tea_with_50_baht():
     assert drink.value == 'Tea'
     assert change.value == 30.0
 
+
 def test_should_return_coffee_change_70_baht_when_buying_coffee_with_100_baht():
-    shop = Shop()
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
     order = DrinkName(value='Coffee')
     pay = MoneyTHB(value=100.0)
 
@@ -25,17 +29,40 @@ def test_should_return_coffee_change_70_baht_when_buying_coffee_with_100_baht():
     assert drink.value == 'Coffee'
     assert change.value == 70.0
 
+
 def test_should_raise_DrinkNotInMenu_when_buying_cocoa():
-    shop = Shop()
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
     order = DrinkName(value='Cocoa')
     pay = MoneyTHB(value=50.0)
 
     with raises(DrinkNotInMenu):
         shop.buy(order, pay)
 
+
 def test_should_raise_MoneyNotEnough_when_buying_coffee_with_1_baht():
-    shop = Shop()
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
     order = DrinkName(value='Coffee')
     pay = MoneyTHB(value=1.0)
+    with raises(MoneyNotEnough):
+        shop.buy(order, pay)
+
+
+def test_should_randomly_select_a_drink_between_tea_and_coffee_when_buying_surprise():
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
+    order = DrinkName(value='Surprise')
+    pay = MoneyTHB(value=100.0)
+    shop.buy(order, pay)
+    assert mock_selector.is_called_with_choices == ['Tea', 'Coffee']
+
+
+def test_should_return_ovaltine_change_15_baht_when_buying_ovaltine_with_15_baht():
+    mock_selector = MockRandomlySelect()
+    shop = Shop(randomly_select=mock_selector)
+    order = DrinkName(value='Ovaltine')
+    pay = MoneyTHB(value=5.0)
+
     with raises(MoneyNotEnough):
         shop.buy(order, pay)
