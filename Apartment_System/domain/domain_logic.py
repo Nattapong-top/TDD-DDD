d51697a52860783c40600a2837ba91290398fd55
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-03 17:53:57 +0700

    feat: should create electricity unit whit valid value and should raise error when unit is negative

--- Apartment_System/domain/domain_logic.py
+++ Apartment_System/domain/domain_logic.py
@@ -1,1 +1,8 @@
 # Domain Logic for Apartment_System
+from pydantic import BaseModel, Field
+
+class ElectricityUnit(BaseModel):
+    value: float = Field(..., gt=0)
+
+    class Config:
+        frozenset = True
--- Apartment_System/tests/test_logic.py
+++ Apartment_System/tests/test_logic.py
@@ -1,1 +1,18 @@
 # Unit Tests for Apartment_System
+from pytest import raises
+from pydantic import ValidationError
+
+from Apartment_System.domain.domain_logic import (
+    ElectricityUnit)
+
+
+def test_should_create_electricity_unit_with_valid_value() -> None:
+    valid_unit = 100.0
+    unit = ElectricityUnit(value=valid_unit)
+    assert unit.value == valid_unit
+
+def test_should_raise_error_when_unit_is_negative() -> None:
+    unit = -10.0
+    with raises(ValidationError):
+        ElectricityUnit(value=unit)
+
