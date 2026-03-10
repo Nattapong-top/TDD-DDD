562484327f459257692ad67323b2142d59a093f5
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-11 06:46:34 +0700

    feat: should raise error MenuItem is Empty and name too long and should raise error when MoneyTHB is negative and Money is more than 1000

--- Restaurant_System/tests/test_value_object.py
+++ Restaurant_System/tests/test_value_object.py
@@ -8,6 +8,22 @@ def test_should_create_MenuItem_is_valid():
     menu_item = MenuItem(value='kaparwkaikidow')
     assert menu_item.value == 'kaparwkaikidow'
 
+def test_should_raise_error_when_Menuitem_is_Empty():
+    with pytest.raises(ValueError):
+        menu_item = MenuItem(value='')
+
+def test_should_raise_error_when_MenuItem_is_name_too_long():
+    with pytest.raises(ValueError):
+        menu_item = MenuItem(value='kaparwkaikidow'*20)
+
 def test_should_create_MoneyTHB_is_valid():
     money_thb = MoneyTHB(value=50.0)
     assert money_thb.value == 50.0
+
+def test_should_raise_error_when_MoneyTHB_is_negative():
+    with pytest.raises(ValueError):
+        money_thb = MoneyTHB(value=-1)
+
+def test_should_raise_error_when_MoneyTHB_is_more_than_1000():
+    with pytest.raises(ValueError):
+        money_thb = MoneyTHB(value=1001)
