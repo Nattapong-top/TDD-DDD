787a7cd963d64ebcab3670e7acf9a1f51eca8987
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-17 22:25:59 +0700

    feat: add PhoneNumber value object with validation
    - normalize hyphen format (081-234-5678 → 0812345678)
    - validate must start with 0, digits only, length 10
    - 13 tests passed

--- Hospital_System/domain/value_object.py
+++ Hospital_System/domain/value_object.py
@@ -13,3 +13,26 @@ class Name(DomainValueObject):
         if v.strip() == '':
             raise ValueError('ชื่อห้ามว่างครับ')
         return v
+
+class PhoneNumber(DomainValueObject):
+    value: str = Field(..., max_length=10, min_length=10)
+
+    @field_validator('value', mode='before')
+    @classmethod
+    def _normalize_phone(cls, v:str) -> str:
+        return v.replace('-', '')
+
+    @field_validator('value', mode='after')
+    @classmethod
+    def _start_not_zero(cls, v:str) -> str:
+        if not v.startswith('0'):
+            raise ValueError('เบอร์โทรขึ้นต้นด้วย 0 เท่านั้นครับ')
+        return v
+
+    @field_validator('value')
+    @classmethod
+    def _must_be_digits(cls, v:str) -> str:
+        if not v.isdigit():
+            raise ValueError('ห้ามมีตัวอักษรครับ')
+        return v
+
--- Hospital_System/tests/test_value_object.py
+++ Hospital_System/tests/test_value_object.py
@@ -2,7 +2,7 @@
 from pydantic import ValidationError
 from pytest import raises, fixture, approx
 
-from Hospital_System.domain.value_object import Name
+from Hospital_System.domain.value_object import Name, PhoneNumber
 
 
 def test_should_create_Name_is_valid():
@@ -31,3 +31,31 @@ def test_should_raise_error_Name_is_whitespace():
 def test_should_raise_error_Name_is_str_and_number():
     with raises(ValueError):
         Name(value='ชื่อภาษาไทยบวกตัวเลข1')
+
+def test_should_create_PhoneNumber_is_valid():
+    phone = PhoneNumber(value='0123456789')
+    assert phone.value == '0123456789'
+
+def test_should_raise_error_PhoneNumber_is_too_long():
+    with raises(ValueError):
+        PhoneNumber(value='05456123456789')
+
+def test_should_raise_error_PhoneNumber_is_emtpy():
+    with raises(ValueError):
+        PhoneNumber(value='')
+
+def test_should_raise_error_PhoneNumber_is_whitespace():
+    with raises(ValueError):
+        PhoneNumber(value='   ')
+
+def test_should_raise_error_PhoneNumber_is_str_and_number():
+    with raises(ValueError):
+        PhoneNumber(value='0234567dfg')
+
+def test_should_create_PhoneNuber_is_Hyphen_and_number():
+    phone = PhoneNumber(value='012-345-6789')
+    assert phone.value == '0123456789'
+
+def test_should_raise_PhoneNumber_start_Not_zero():
+    with raises(ValueError):
+        PhoneNumber(value='9123456789')
