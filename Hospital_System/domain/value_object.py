f83b2d1f46557801def838ec0b68ad6374a52fdc
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-20 23:05:52 +0700

    feat: add Diagnosis value object with basic validator - 4 test passed

--- Hospital_System/domain/value_object.py
+++ Hospital_System/domain/value_object.py
@@ -115,3 +115,15 @@ class VitalSigns(DomainValueObject):
         if v.strip() == '':
             raise ValueError('กรุณาบอกอาการด้วย')
         return v
+
+class Diagnosis(DomainValueObject):
+    disease: str = Field(..., min_length=1, max_length=100)
+    treatment: str = Field(..., min_length=1, max_length=100)
+    medicine_prescribed: list[str] = []
+
+    @field_validator('disease', 'treatment')
+    @classmethod
+    def _must_not_be_blank(cls, v: str) -> str:
+        if v.strip() == '':
+            raise ValueError('กรุณากรอกข้อมูลด้วยครับ')
+        return v
--- Hospital_System/tests/test_value_object.py
+++ Hospital_System/tests/test_value_object.py
@@ -4,7 +4,7 @@ from pytest import raises, fixture, approx
 from datetime import date
 from Hospital_System.domain.value_object import (
     Name, PhoneNumber, DateOfBirth, Address, Province, PatientRights, Rights, BloodPressure, Weight, Height,
-    Temperature, DomainValueObject, VitalSigns)
+    Temperature, DomainValueObject, VitalSigns, Diagnosis)
 
 
 # ส่วนของ VO Name เทสชื่อและนามสกุล
@@ -15,54 +15,67 @@ def test_should_create_Name_is_valid():
     assert first_name.value == 'นนทพัฒน์'
     assert last_name.value == 'คนสุขภาพดี'
 
+
 def test_should_raise_error_Name_is_too_long():
     with raises(ValueError):
-        Name(value='นนทพัฒน์'*20)
+        Name(value='นนทพัฒน์' * 20)
+
 
 def test_should_raise_error_Name_is_emtpy():
     with raises(ValueError):
         Name(value='')
 
+
 def test_should_raise_error_Name_is_number():
     with raises(ValueError):
         Name(value='123456789')
+
 
 def test_should_raise_error_Name_is_whitespace():
     with raises(ValueError):
         Name(value='       ')
 
+
 def test_should_raise_error_Name_is_str_and_number():
     with raises(ValueError):
         Name(value='ชื่อภาษาไทยบวกตัวเลข1')
+
 
 # ส่วนของ VO PhoneNumber เทสเบอร์โทรศัพท์
 def test_should_create_PhoneNumber_is_valid():
     phone = PhoneNumber(value='0123456789')
     assert phone.value == '0123456789'
 
+
 def test_should_raise_error_PhoneNumber_is_too_long():
     with raises(ValueError):
         PhoneNumber(value='05456123456789')
+
 
 def test_should_raise_error_PhoneNumber_is_emtpy():
     with raises(ValueError):
         PhoneNumber(value='')
 
+
 def test_should_raise_error_PhoneNumber_is_whitespace():
     with raises(ValueError):
         PhoneNumber(value='   ')
+
 
 def test_should_raise_error_PhoneNumber_is_str_and_number():
     with raises(ValueError):
         PhoneNumber(value='0234567dfg')
 
+
 def test_should_create_PhoneNumber_is_Hyphen_and_number():
     phone = PhoneNumber(value='012-345-6789')
     assert phone.value == '0123456789'
 
+
 def test_should_raise_PhoneNumber_start_Not_zero():
     with raises(ValueError):
         PhoneNumber(value='9123456789')
+
 
 # ส่วนของ VO Age เทสอายุ
 def test_should_create_DateOfBirth_is_valid():
@@ -71,22 +84,27 @@ def test_should_create_DateOfBirth_is_valid():
     assert dob.month == 12
     assert dob.day == 31
 
+
 def test_should_raise_error_DateOfBirth_month_out_of_range():
     with raises(ValueError):
         DateOfBirth(year=1990, month=13, day=31)
+
 
 def test_should_raise_error_DateOfBirth_day_out_of_range():
     with raises(ValueError):
         DateOfBirth(year=1990, month=12, day=32)
 
+
 def test_should_raise_error_DateOfBirth_is_future_dae():
     with raises(ValueError):
         DateOfBirth(year=2026, month=12, day=31)
+
 
 def test_should_raise_error_DateOfBirth_is_Over_150_years():
     today = date.today()
     with raises(ValueError):
         DateOfBirth(year=today.year - 151, month=12, day=31)
+
 
 def test_should_create_DateOfBirth_is_today():
     today = date.today()
@@ -94,6 +112,7 @@ def test_should_create_DateOfBirth_is_today():
     assert dob.year == today.year
     assert dob.month == today.month
     assert dob.day == today.day
+
 
 # ส่วนของ VO Address เทสที่อยู่
 def test_should_create_registered_Address_is_valid():
@@ -113,6 +132,7 @@ def test_should_create_registered_Address_is_valid():
     assert registered_address.province == Province.MUKDAHAN
     assert registered_address.postal_code == '49000'
 
+
 def test_should_create_current_Address_is_valid():
     current_address = Address(  # ตั้งอยู่ที่ 173 ถนนดินสอ แขวงเสาชิงช้า เขตพระนคร กรุงเทพมหานคร 10200
         house_number='173',
@@ -129,6 +149,7 @@ def test_should_create_current_Address_is_valid():
     assert current_address.province == Province.BANGKOK
     assert current_address.postal_code == '10200'
 
+
 def test_should_raise_error_when_province_is_invalid_NotInEnum():
     with raises(ValueError):
         Address(
@@ -140,6 +161,7 @@ def test_should_raise_error_when_province_is_invalid_NotInEnum():
             postal_code='49000'
         )
 
+
 def test_should_raise_error_when_postal_code_is_invalid_str_over_5():
     with raises(ValueError):
         registered_address = Address(
@@ -150,6 +172,8 @@ def test_should_raise_error_when_postal_code_is_invalid_str_over_5():
             province=Province.MUKDAHAN,
             postal_code='49000dfg'
         )
+
+
 def test_should_create_Address_when_without_street_is_valid():
     address = Address(
         house_number='10',
@@ -161,106 +185,132 @@ def test_should_create_Address_when_without_street_is_valid():
     )
     assert address.street is None
 
+
 def test_should_create_PatientRights_is_valid():
     rights = PatientRights.GOLD_CARD
     assert rights == PatientRights.GOLD_CARD
+
 
 def test_should_raise_error_when_PatientRights_is_invalid_NotInEnum():
     with raises(ValueError):
         PatientRights('บัตรหมดอายุ')
 
+
 def test_should_create_Rights_is_valid():
     rights = Rights(rights_type=PatientRights.COMPANY_INSURANCE)
     assert rights.rights_type == PatientRights.COMPANY_INSURANCE
 
+
 def test_should_raise_error_when_PatientRights_type_is_invalid_NotInEnum():
     with raises(ValueError):
         Rights(rights_type='บัตรดำ')
+
 
 # ส่วนของ VO BloodPressure เทสวัดชีพจร
 def test_should_create_BloodPressure_is_valid():
     blood_pressure = BloodPressure(systolic=120, diastolic=80)
     assert blood_pressure == BloodPressure(systolic=120, diastolic=80)
 
+
 def test_should_raise_error_when_BloodPressure_systolic_over_of_range():
     with raises(ValueError):
         BloodPressure(systolic=200, diastolic=80)
+
 
 def test_should_raise_error_when_BloodPressure_systolic_lower_of_range():
     with raises(ValueError):
         BloodPressure(systolic=120, diastolic=8)
 
+
 def test_should_raise_error_when_BloodPressure_diastolic_over_range():
     with raises(ValueError):
         BloodPressure(systolic=120, diastolic=91)
 
+
 def test_should_raise_error_when_BloodPressure_diastolic_lower_of_range():
     with raises(ValueError):
         BloodPressure(systolic=120, diastolic=9)
+
 
 # ส่วนของ VO Weight เทสน้ำหนัก
 def test_should_create_Weight_is_valid():
     weight = Weight(value=80)
     assert weight == Weight(value=80)
 
+
 def test_should_raise_error_when_Weight_over_of_range():
     with raises(ValueError):
         Weight(value=301)
+
 
 def test_should_raise_error_when_Weight_lower_of_range():
     with raises(ValueError):
         Weight(value=0.1)
 
+
 def test_should_raise_error_when_Weight_input_str():
     with raises(ValueError):
         Weight(value='สิบ')
 
+
 def test_should_raise_error_when_Weight_is_negative():
     with raises(ValueError):
         Weight(value=-1)
+
 
 # ส่วนของ VO Height เทสส่วนสูง
 def test_should_create_Height_is_valid():
     height = Height(value=80)
     assert height == Height(value=80)
 
+
 def test_should_raise_error_when_Height_over_of_range():
     with raises(ValueError):
         Height(value=301)
+
 
 def test_should_raise_error_when_Height_lower_of_range():
     with raises(ValueError):
         Height(value=0.1)
 
+
 def test_should_raise_error_when_Height_input_str():
     with raises(ValueError):
         Height(value='สิบ')
 
+
 def test_should_raise_error_when_Height_is_negative():
     with raises(ValueError):
         Height(value=-1)
+
 
 # ส่วนของ VO Temperature เทสอุณหภูมิร่างกาย
 def test_should_create_Temperature_is_valid():
     temp = Temperature(value=35.0)
     assert temp == Temperature(value=35)
 
+
 def test_should_raise_error_when_Temperature_over_of_range():
     with raises(ValueError):
         Temperature(value=301)
+
 
 def test_should_raise_error_when_Temperature_lower_of_range():
     with raises(ValueError):
         Temperature(value=0.1)
 
+
 def test_should_raise_error_when_Temperature_input_str():
     with raises(ValueError):
         Temperature(value='สิบ')
+
 
 def test_should_raise_error_when_Temperature_is_negative():
     with raises(ValueError):
         Temperature(value=-1)
 
+
+# ส่วนของ VO VitalSigns เทสอาการผู้ป่วย
 @fixture
 def vital_signs() -> VitalSigns:
     return VitalSigns(
@@ -271,6 +321,7 @@ def vital_signs() -> VitalSigns:
         symptom='น้ำหมูกไหล ปวดหัว ตัวร้อน หนาวสั่น'
     )
 
+
 def test_should_create_VitalSigns_is_valid(vital_signs):
     assert vital_signs == VitalSigns(
         blood_pressure=BloodPressure(systolic=120, diastolic=80),
@@ -280,15 +331,17 @@ def test_should_create_VitalSigns_is_valid(vital_signs):
         symptom='น้ำหมูกไหล ปวดหัว ตัวร้อน หนาวสั่น'
     )
 
+
 def test_should_raise_error_when_VitalSigns_symptom_empty_and_whitespace():
     with raises(ValueError):
         VitalSigns(
-        blood_pressure=BloodPressure(systolic=120, diastolic=80),
-        weight=Weight(value=80),
-        height=Height(value=177),
-        temperature=Temperature(value=39.0),
-        symptom='      '
+            blood_pressure=BloodPressure(systolic=120, diastolic=80),
+            weight=Weight(value=80),
+            height=Height(value=177),
+            temperature=Temperature(value=39.0),
+            symptom='      '
         )
+
 
 def test_should_raise_error_when_VitalSigns_symptom_too_long():
     with raises(ValueError):
@@ -297,5 +350,53 @@ def test_should_raise_error_when_VitalSigns_symptom_too_long():
             weight=Weight(value=80),
             height=Height(value=177),
             temperature=Temperature(value=39.0),
-            symptom='น้ำหมูกไหล ปวดหัว ตัวร้อน หนาวสั่น'*20
+            symptom='น้ำหมูกไหล ปวดหัว ตัวร้อน หนาวสั่น' * 20
         )
+
+
+# ส่วนของ VO Diagnosis_ เทสคำวินิจฉัย
+@fixture
+def diagnosis() -> Diagnosis:
+    return Diagnosis(
+        disease='ไข้หวัดใหญ่',
+        treatment='พักผ่อนน ดิ่มน้ำมากๆ',
+        medicine_prescribed=['Paracetamol', 'Loratadine']
+    )
+
+
+def test_create_Diagnosis_is_valid(diagnosis):
+    assert diagnosis == Diagnosis(
+        disease='ไข้หวัดใหญ่',
+        treatment='พักผ่อนน ดิ่มน้ำมากๆ',
+        medicine_prescribed=['Paracetamol', 'Loratadine']
+    )
+
+
+def test_should_raise_error_when_Diagnosis_empty_and_whitespace():
+    with raises(ValueError):
+        Diagnosis(
+            disease='   ',
+            treatment='   ',
+            medicine_prescribed=['Paracetamol', 'Loratadine']
+        )
+
+
+def test_should_create_Diagnosis_without_medicine_prescribed():
+    diagnosis = Diagnosis(
+        disease='เครียดสะสม',
+        treatment='พักผ่อน ออกกำลังกาย',
+        medicine_prescribed=[]  #ไม่มียา
+    )
+    assert diagnosis == Diagnosis(
+        disease='เครียดสะสม',
+        treatment='พักผ่อน ออกกำลังกาย',
+        medicine_prescribed=[]  # ไม่มียา
+    )
+
+def test_should_raise_error_when_Diagnosis_max_length():
+    with raises(ValueError):
+        Diagnosis(
+        disease='เครียดสะสม'*30,
+        treatment='พักผ่อน ออกกำลังกาย'*30,
+        medicine_prescribed=[]  # ไม่มียา
+    )
