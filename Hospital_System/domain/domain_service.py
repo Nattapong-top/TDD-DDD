a078c48a34592c8cfa6ffdf73224c7f5002e9d0c
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-03-29 00:21:44 +0700

    feat(domain): ปรับปรุงการคำนวณคิวใน QueueService - เปลี่ยนชื่อตัวแปรจาก current_date เป็น last_date เพื่อความชัดเจน - เพิ่ม method reset คิวเป็น 1 เมื่อวันที่ล่าสุด (last_date) เก่ากว่า วันที่ปัจจุบัน (today) - อัปเดตเทสต์เคสทั้งหมดให้รองรับการส่งค่าพารามิเตอร์แบบใหม่

--- Hospital_System/domain/domain_service.py
+++ Hospital_System/domain/domain_service.py
@@ -1,12 +1,19 @@
 # domain_service.py
 from typing import Tuple
 
-from pydantic import Field
 from datetime import datetime, date
 
 from Hospital_System.domain.value_object import Number
 
 
 class QueueService:
-    def get_next_number(self, last_number:Number, current_date: date, time_now: datetime) -> Tuple[Number,date, datetime]:
-        return Number(id=last_number.id + 1), current_date, time_now
+    def get_next_number(self, last_number:Number, last_date: date, time_now: datetime, today: date) -> Tuple[Number,date, datetime]:
+        last_number, last_date = self._reset_date_and_number(last_number, last_date, today)
+        now_number = Number(id=last_number.id + 1)
+        return now_number, last_date, time_now
+
+    def _reset_date_and_number(self, last_number: Number, last_date: date, today: date) -> tuple[Number, date]:
+        if last_date < today:
+            last_date = today
+            last_number = Number(id=0)
+        return last_number, last_date
--- Hospital_System/tests/test_domain_service.py
+++ Hospital_System/tests/test_domain_service.py
@@ -1,21 +1,34 @@
 # test_domain_service.py
-import datetime
-import time
-
-import pytest
+from pytest import fixture
 from datetime import date, datetime
 from Hospital_System.domain.domain_service import QueueService
 from Hospital_System.domain.value_object import Number
 
+@fixture
+def today_date() -> date:
+    return date(2026, 3, 28)
+
+@fixture
+def now() -> datetime:
+    return datetime(2026, 3, 28,
+                    22, 43, 53, 302903)
 
 def test_should_queue_service_issue_first_number_of_the_day():
     service = QueueService()
 
     today = date(2026, 3, 28)
     now = datetime(2026, 3, 28, 22, 43, 53, 302903)
-    next_number, current_date, time_now = service.get_next_number(
-        last_number=Number(id=0), current_date=today, time_now=now)
+    next_number, last_date, time_now = service.get_next_number(
+        last_number=Number(id=0), last_date=today, time_now=now, today=today)
     assert next_number == Number(id=1)
-    assert current_date == date(2026, 3, 28)
+    assert last_date == date(2026, 3, 28)
     assert time_now == datetime(2026, 3, 28, 22, 43, 53, 302903)
 
+def test_should_queue_service_reset_number_and_last_date(today_date, now):
+    service = QueueService()
+    next_number, last_date, time_now = service.get_next_number(
+        last_number=Number(id=99), last_date=date(2026, 3, 27),
+        time_now=now, today=today_date)
+    assert next_number == Number(id=1)
+    assert last_date == date.today()
+    assert time_now == now
